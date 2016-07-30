748610eed3f1441ddd6156d26484b3d4b9a3a5f6
kaba32 <m07112@gmail.com>
2016-07-31 03:56:49 +0900

    初期設定を追加

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -8,21 +8,29 @@ import numpy as np
 
 
 class KalmanFilter(object):
-    def __init__(self, transition_matrix, transition_covariance, observe_matrix, transition, observe_cobariance,
+    def __init__(self, transition_matrix, transition_covariance, observe_matrix, transition, observe_covariance,
                  init_state):
+        self.state_dim = init_state.shape[0]
         # 遷移行列
         self.A = transition_matrix
         # 観測行列
         self.C = observe_matrix
         # 遷移確率の分散行列
-        self.Gamma = observe_cobariance
+        self.Gamma = observe_covariance
         # 観測確率の分散行列
-        self.Sigma = observe_cobariance
-        # 初期状態
-        self.state = init_state
+        self.Sigma = observe_covariance
+        # 事前状態ベクトル
+        self.priori_state = init_state
+        # 事前共分散行列
 
-    def fit(self):
-        pass
+        self.priori_state = np.ranodm.normal(size=0)
+        self.priori_covariance = np.random.normal(size=self.Gamma.shape)
+        # カルマンゲイン
+        self.kalman_gain = None
+        # 事後状態ベクトル
+        self.posteriori_state = self.prior_state
+        # 事後共分散行列
+        self.posteriori_covariance = self.prior_covariance
 
-    def predict(self, n):
+    def predict(self, observerd_data, n):
         pass
