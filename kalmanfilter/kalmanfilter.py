23014429e83d87a200ceae88f02efc84cdb250f5
kaba32 <m07112@gmail.com>
2016-07-31 03:34:32 +0900

    初期化処理の追加

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -1,14 +1,28 @@
 # coding:utf-8
+# カルマンフィルタの実装
+# このバージョンでは予め遷移行列・観測行列を指定する必要があります
+# このプログラムは与えられた時系列に対し、n step後の時系列を確率的に推論することができます
+#
 
 import numpy as np
 
 
 class KalmanFilter(object):
-    def __init__(self, transition_matrix, observe_matrix,transition_):
-        pass
+    def __init__(self, transition_matrix, transition_covariance, observe_matrix, transition, observe_cobariance,
+                 init_state):
+        # 遷移行列
+        self.A = transition_matrix
+        # 観測行列
+        self.C = observe_matrix
+        # 遷移確率の分散行列
+        self.Gamma = observe_cobariance
+        # 観測確率の分散行列
+        self.Sigma = observe_cobariance
+        # 初期状態
+        self.state = init_state
 
     def fit(self):
         pass
 
-    def predict(self):
+    def predict(self, n):
         pass
