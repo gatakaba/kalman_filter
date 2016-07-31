41edf01e89462618152af08d8beffd31ef346b6d
kaba32 <m07112@gmail.com>
2016-08-01 04:15:16 +0900

    add function _update

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -16,7 +16,30 @@ class KalmanFilter(object):
         self.R  # 観測ノイズの分散共分散行列
 
         self.K  # カルマンゲイン
+        self.m  # 状態推定値
         self.P  # 推定誤差分散共分散行列
 
-    def update(self, observerd_data, n):
-        pass
+    def _update(self, observerd_data):
+        """
+        観測されたデータに応じて、状態と推定共分散行列を更新する
+        :param observerd_data (1d ndarray): 観測値
+        :return: self
+        """
+        x = observerd_data
+        F, H = self.F, self.H
+        Q, R = self.Q, self.R
+        m = np.copy(self.m)
+        P = np.copy(self.P)
+
+        # prediction step
+        m = F @ m
+        P = F @ P @ F.T + Q
+        # update step
+        S = H @ P @ H.T + R
+        K = P @ H.T @ np.linalg.inv(S)
+        m = m + K @ (x - H @ m)
+        P = P - K @ S @ K.T
+
+        self.m = m
+        self.P = P
+        return self
