b85d6c995010d96b0029a10126039ba059d73106
gatakaba <m07112@gmil.com>
2016-08-03 17:46:01 +0900

    add comment

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -1,21 +1,36 @@
 # coding:utf-8
 """
-# カルマンフィルタの実装
+=====================================
+Inference for Dynamic Linear Systems
+=====================================
 
-## モデル
-    状態方程式
-        * p(z_{n+1}|z_{n}) = N(z_{n+1}|F z_{n},\gamma)
-    観測方程式
-        * p(x_{n}|z_{n}) = N(x_{n}|C z_{n},\sigma)
-## 機能
-    * 状態推定(フィルタリング)
-    * 状態予測
-    * 観測値の予測
+# features
+* estimate current state
+* predict after k step state
+* predict after k step observation
+
 """
 import numpy as np
 
 
 class KalmanFilter(object):
+    """カルマンフィルタの実装
+        * p(z_{n+1}|z_{n}) = N(z_{n+1}|F z_{n},\gamma)
+        * p(x_{n}|z_{n}) = N(x_{n}|C z_{n},\sigma)
+
+    Parameters
+    ----------
+    transition_matrix : ndarray, shape = (state_dim, state_dim)
+
+    observation_matrix : ndarray, shape = (observation_dim, state_dim)
+
+    process_noise : ndarray, shape = (state_dim, state_dim)
+        also known as process_covariance_matrix
+
+    observation_noise : ndarray, shape = (observation_dim, ovservation_dim)
+        also known as observation_covariance_matrix
+    """
+
     def __init__(self, transition_matrix, observation_matrix, process_noise, observation_noise):
         self.F = transition_matrix  # 遷移行列
         self.H = observation_matrix  # 観測行列
@@ -23,13 +38,24 @@ class KalmanFilter(object):
         self.Q = process_noise  # プロセスノイズの分散共分散行列
         self.R = observation_noise  # 観測ノイズの分散共分散行列
 
-        self.m = np.random.normal(size=4)  # 状態推定値
+        self.m = np.random.normal(size=4)  # 状態量の条件付き期待値
         self.P = np.eye(4)  # 推定誤差分散共分散行列
 
         self.K = None  # カルマンゲイン
 
+        self.M = self.F.shape[0]  # 状態量の次元数
+        self.N = self.H.shape[0]  # 観測値の次元数
+
     @property
-    def get_state(self):
+    def state_dim(self):
+        return self.M
+
+    @property
+    def observation_dim(self):
+        return self.N
+
+    @property
+    def current_state(self):
         return self.m, self.P
 
     @property
@@ -41,19 +67,21 @@ class KalmanFilter(object):
         return self.H
 
     @property
-    def process_covariance_matrix(self):
+    def process_noise(self):
         return self.Q
 
     @property
-    def observation_covariance_matrix(self):
+    def observation_noise(self):
         return self.R
 
     def update(self, observerd_data):
-        """
-        p(z_{t}|x_{1:t})
-        観測されたデータに応じて、状態と推定共分散行列を更新する
-        :param 観測値 (1d ndarray)
-        :return: 状態の確率密度関数の平均値と分散
+        """ update state
+
+        推定した状態量を取得したい場合、current_stateにアクセスして下さい (← bad comment)
+
+        Parameters
+        ----------
+        observerd_data : ndarray ,shape = (observation_dim)
         """
 
         x = observerd_data
