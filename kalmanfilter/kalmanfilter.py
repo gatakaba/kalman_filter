f0df4a37e90cc028ac64a6c5f6c61f60ad205ac5
gatakaba <m07112@gmil.com>
2016-08-03 14:11:53 +0900

    add test code

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -6,16 +6,16 @@ import numpy as np
 
 class KalmanFilter(object):
     def __init__(self, dim_x, dim_z):
-        self.z  # 状態
-        self.F  # 遷移行列
-        self.H  # 観測行列
+        self.z = None  # 状態
+        self.F = None  # 遷移行列
+        self.H = None  # 観測行列
 
-        self.Q  # プロセスノイズの分散共分散行列
-        self.R  # 観測ノイズの分散共分散行列
+        self.Q = None  # プロセスノイズの分散共分散行列
+        self.R = None  # 観測ノイズの分散共分散行列
 
-        self.K  # カルマンゲイン
-        self.m  # 状態推定値
-        self.P  # 推定誤差分散共分散行列
+        self.K = None  # カルマンゲイン
+        self.m = None  # 状態推定値
+        self.P = None  # 推定誤差分散共分散行列
 
     def _update(self, observerd_data):
         """
@@ -43,3 +43,53 @@ class KalmanFilter(object):
         return self.m
 
 
+if __name__ == "__main__":
+    dt = 10 ** -3
+    sigma1 = 0.5
+    sigma2 = 0.5
+    q1 = 0.5
+    q2 = 0.5
+    # transition matrix
+    A = np.array([[1, 0, dt, 0],
+                  [0, 1, 0, dt],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+    # process noise covariance
+    Q = np.array([[q1 * dt ** 3 / 3, 0, q1 * dt ** 2 / 2, 0],
+                  [0, q1 * dt ** 3 / 3, 0, q2 * dt ** 2 / 2],
+                  [q1 * dt ** 2 / 2, 0, q1 * dt, 0],
+                  [0, q2 * dt ** 2 / 2, 0, q2 * dt]])
+
+    # observe matrix
+    H = np.array([[1, 0, 0, 0],
+                  [0, 1, 0, 0]])
+    # measurement noise covariance
+    R = np.array([[sigma1 ** 2, 0],
+                  [0, sigma2 ** 2]])
+
+    m = np.random.normal(size=4)
+    P = np.eye(4)
+
+    # set parameter
+    kf = KalmanFilter(4, 2)
+
+    kf.F = A
+    kf.H = H
+
+    kf.Q = Q
+    kf.R = R
+
+    kf.m = m
+    kf.P = P
+
+    trajectory = np.random.normal(size=[1000, 2])
+    state_list = []
+    var_list = []
+
+    for y in trajectory:
+        state_list.append(np.copy(m))
+        var_list.append(np.array(P[0, 0]))
+
+    X = np.array(state_list)
+    v = np.array(var_list)
+    print(X)
