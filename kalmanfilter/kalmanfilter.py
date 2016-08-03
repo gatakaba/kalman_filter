0b71b8b91ada2cd3b3cedb2a05c77c98e98e5cf7
gatakaba <m07112@gmil.com>
2016-08-03 17:04:47 +0900

    初期化処理を改善

--- kalmanfilter/kalmanfilter.py
+++ kalmanfilter/kalmanfilter.py
@@ -4,30 +4,32 @@
 
 ## モデル
     状態方程式
-        * p(z_{n+1}|z_{n}) = N(z_{n+1}|F z_{n},\gamma)= N(z_{n+1}|n_{n},\gamma)
+        * p(z_{n+1}|z_{n}) = N(z_{n+1}|F z_{n},\gamma)
     観測方程式
-        * p(x_{n}|z_{n}) = N(x_{n}|C z_{n},\sigma) = N(x_{n}|z_{n},\sigma)
+        * p(x_{n}|z_{n}) = N(x_{n}|C z_{n},\sigma)
 ## 機能
     * 状態推定(フィルタリング)
     * 状態予測
     * 観測値の予測
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class KalmanFilter(object):
-    def __init__(self, dim_x, dim_z):
-        self.F = None  # 遷移行列
-        self.H = None  # 観測行列
+    def __init__(self, transition_matrix, observation_matrix, process_noise, observation_noise):
+        self.F = transition_matrix  # 遷移行列
+        self.H = observation_matrix  # 観測行列
 
-        self.Q = None  # プロセスノイズの分散共分散行列
-        self.R = None  # 観測ノイズの分散共分散行列
+        self.Q = process_noise  # プロセスノイズの分散共分散行列
+        self.R = observation_noise  # 観測ノイズの分散共分散行列
 
         self.K = None  # カルマンゲイン
-        self.m = None  # 状態推定値
-        self.P = None  # 推定誤差分散共分散行列
+
+        self.m = np.random.normal(size=4)  # 状態推定値
+        # self.P = np.random.normal(size=[4, 4])
+        self.P = np.eye(4)  # 推定誤差分散共分散行列
 
     @property
     def get_state(self):
@@ -86,70 +88,4 @@ class KalmanFilter(object):
     def predict_observation(self, k):
         # p(x_{t+k}|x_{1:k})
         # estimate observation after k step
-
         pass
-
-
-if __name__ == "__main__":
-    dt = 10 ** -1
-    sigma1 = 0.5
-    sigma2 = 0.5
-    q1 = 0.5
-    q2 = 0.5
-    # transition matrix
-    A = np.array([[1, 0, dt, 0],
-                  [0, 1, 0, dt],
-                  [0, 0, 1, 0],
-                  [0, 0, 0, 1]])
-
-    # process noise covariance
-    Q = np.array([[q1 * dt ** 3 / 3, 0, q1 * dt ** 2 / 2, 0],
-                  [0, q1 * dt ** 3 / 3, 0, q2 * dt ** 2 / 2],
-                  [q1 * dt ** 2 / 2, 0, q1 * dt, 0],
-                  [0, q2 * dt ** 2 / 2, 0, q2 * dt]])
-
-    # observe matrix
-    H = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, 0]])
-    # measurement noise covariance
-    R = np.array([[sigma1 ** 2, 0],
-                  [0, sigma2 ** 2]])
-
-    m = np.random.normal(size=4)
-    P = np.eye(4) * 10
-
-    # set parameter
-    kf = KalmanFilter(4, 2)
-
-    kf.F = A
-    kf.H = H
-
-    kf.Q = Q
-    kf.R = R
-
-    kf.m = m
-    kf.P = P
-
-    t = np.linspace(0, 30, 1000)
-    trajectory = np.c_[np.cos(t) + t, np.sin(t) * t]
-
-    trajectory += np.random.normal(0, 1, size=trajectory.shape)
-    state_list = []
-    var_list = []
-
-    for y in trajectory:
-        kf.update(y)
-        m, P = kf.get_state
-        state_list.append(np.copy(m))
-        var_list.append(np.copy(P[0, 0]))
-
-    X = np.array(state_list)
-    v = np.array(var_list)
-
-    plt.scatter(X[:, 0], X[:, 1], s=v ** 0.5 * 10, alpha=0.25)
-
-    plt.plot(trajectory[:, 0], trajectory[:, 1], "bo", label="observerd")
-    plt.plot(X[:, 0], X[:, 1], "r-", linewidth=5, label="estimated")
-    plt.legend()
-
-    plt.show()
--- kalmanfilter/my_test.py
+++ kalmanfilter/my_test.py
@@ -1,5 +1,4 @@
 # coding:utf-8
-from kalmanfilter import KalmanFilter
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,10 +8,14 @@ sigma2 = 0.5
 q1 = 0.5
 q2 = 0.5
 # transition matrix
-A = np.array([[1, 0, dt, 0],
+F = np.array([[1, 0, dt, 0],
               [0, 1, 0, dt],
               [0, 0, 1, 0],
               [0, 0, 0, 1]])
+
+# observe matrix
+H = np.array([[1, 0, 0, 0],
+              [0, 1, 0, 0]])
 
 # process noise covariance
 Q = np.array([[q1 * dt ** 3 / 3, 0, q1 * dt ** 2 / 2, 0],
@@ -20,27 +23,12 @@ Q = np.array([[q1 * dt ** 3 / 3, 0, q1 * dt ** 2 / 2, 0],
               [q1 * dt ** 2 / 2, 0, q1 * dt, 0],
               [0, q2 * dt ** 2 / 2, 0, q2 * dt]])
 
-# observe matrix
-H = np.array([[1, 0, 0, 0],
-              [0, 1, 0, 0]])
 # measurement noise covariance
 R = np.array([[sigma1 ** 2, 0],
               [0, sigma2 ** 2]])
 
-m = np.random.normal(size=4)
-P = np.eye(4) * 10
-
 # set parameter
-kf = KalmanFilter(4, 2)
-
-kf.F = A
-kf.H = H
-
-kf.Q = Q
-kf.R = R
-
-kf.m = m
-kf.P = P
+kf = KalmanFilter(F, H, Q, R)
 
 t = np.linspace(0, 30, 1000)
 trajectory = np.c_[np.cos(t) + t, np.sin(t) * t]
@@ -50,17 +38,18 @@ state_list = []
 var_list = []
 
 for y in trajectory:
-    m, P = kf.update(y)
+    kf.update(y)
+    m, P = kf.get_state
     state_list.append(np.copy(m))
     var_list.append(np.copy(P[0, 0]))
 
 X = np.array(state_list)
 v = np.array(var_list)
 
-plt.scatter(X[:, 0], X[:, 1], s=v ** 0.5 * 10, alpha=0.25)
+# plt.scatter(X[:, 0], X[:, 1], s=v ** 0.5 * 10, alpha=0.25)
 
 plt.plot(trajectory[:, 0], trajectory[:, 1], "bo", label="observerd")
 plt.plot(X[:, 0], X[:, 1], "r-", linewidth=5, label="estimated")
 plt.legend()
 
-# plt.show()
+plt.show()
