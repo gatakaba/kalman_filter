d44c8062b24d9f227e7d9c226cbabb9bbee8be48
kaba32 <m07112@gmail.com>
2016-08-05 19:01:09 +0900

    prediction test

--- kalmanfilter/my_test.py
+++ kalmanfilter/my_test.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 from kalmanfilter import KalmanFilter
 
 dt = 10 ** -1
-sigma1 = 0.5
-sigma2 = 0.5
-q1 = 0.5
-q2 = 0.5
+sigma1 = 10
+sigma2 = 10
+q1 = 10
+q2 = 10
 # transition matrix
 F = np.array([[1, 0, dt, 0],
               [0, 1, 0, dt],
@@ -32,7 +32,9 @@ R = np.array([[sigma1 ** 2, 0],
 kf = KalmanFilter(F, H, Q, R)
 
 t = np.linspace(0, 30, 1000)
-trajectory = np.c_[np.cos(t) + t, np.sin(t) * t]
+#trajectory = np.c_[np.cos(t) + t, np.sin(t) * t]
+
+trajectory = np.c_[t,t**2]
 
 trajectory += np.random.normal(0, 1, size=trajectory.shape)
 state_list = []
@@ -44,21 +46,19 @@ for y in trajectory:
     state_list.append(np.copy(m))
     var_list.append(np.copy(P[0, 0]))
 
-print()
-plt.plot(kf.predict_state(10)[0])
-plt.show()
-# print(kf.predict_state(i)[1])
-# print(kf.predict_observation(i))
+for i in range(1,500):
+    m,p=kf.predict_state(i,spot_estimation=True)
+    state_list.append(np.copy(m))
+    var_list.append(np.copy(p[0,0]))
 
-"""
 X = np.array(state_list)
 v = np.array(var_list)
 
-# plt.scatter(X[:, 0], X[:, 1], s=v ** 0.5 * 10, alpha=0.25)
+
+plt.scatter(X[:, 0], X[:, 1], s=v ** 0.5 * 50, alpha=0.25)
 
 plt.plot(trajectory[:, 0], trajectory[:, 1], "bo", label="observerd")
-plt.plot(X[:, 0], X[:, 1], "r-", linewidth=5, label="estimated")
+plt.plot(X[:, 0], X[:, 1], "r-", linewidth=3, label="estimated")
 plt.legend()
 
 plt.show()
-"""
